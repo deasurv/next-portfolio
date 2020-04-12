42f74c335fb5b1a4c1dcc67f58ee9be038e29a16
Majid Fatahi <Majid6670@gmail.com>
2020-04-12 17:24:18 +0430

    added some stuff for blogs and fa

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,20 +1,29 @@
 import React, { Fragment } from 'react';
-
 import Header from './../shared/Header';
+
+import Head from 'next/head';
 
 const BaseLayout = (props) => {
     const { children, className, isAuthenticated, user } = props;
     const headerType = props.headerType || 'default';
     
     return(
-        <div className="layout-container" >
-            <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated} user={user} />
-            <main className={`cover ${className}`}>
-                <div className="wrapper">
-                    {children}
-                </div>
-            </main>
-        </div>
+        <Fragment>
+            <Head>
+                <title>Majid Fatahi</title>
+
+                <link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet" crossorigin="anonymous" />
+                <script src="https://kit.fontawesome.com/5cc96674f7.js" crossorigin="anonymous"></script>
+            </Head>
+            <div className="layout-container" >
+                <Header className={`port-nav-${headerType}`} isAuthenticated={isAuthenticated} user={user} />
+                <main className={`cover ${className}`}>
+                    <div className="wrapper">
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </Fragment>
     );
 }
 
